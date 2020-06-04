2b1b455a499f2af3393db6e20dfd131372a2c6f1
Rajashree610 <rajashreey841@gmail.com>
2020-06-04 03:48:31 -0700

    Adding the add binary program

---
+++ Add_binary.py
@@ -0,0 +1,30 @@
+'''
+problem:Add Binary
+
+Given two binary strings, return their sum (also a binary string).
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+Author:https://github.com/rajashreey841
+'''
+
+
+import os
+import sys
+
+def addBinary(a,b):
+     return str(bin(int(a,2)+int(b,2)))[2:]
+
+if __name__ == "__main__":
+    i = input("Enter the string\t")
+    j = input("Enter the string\t")
+    print("Input:\ta =\"",i ,"\"","b =\"",j,"\"")
+    print("Result:",addBinary(i,j))
+
