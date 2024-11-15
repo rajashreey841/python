ead32ee4a81f4c387d768fd8655eca1df78d1e45
rajashreey841 <rajashreey841@gmail.com>
2024-11-15 14:59:26 -0500

    Adding Random anme program

---
+++ Random_names.py
@@ -0,0 +1,12 @@
+'''
+Program_name: WAP which will select a random name from a list of names and the person selected will have to pay for eveybody's food bill:
+
+'''
+import random
+
+Names = input("Enter the people name: ")
+separate =  Names.split()
+print("list= ", separate)
+result = random.randint(0,len(separate)-1)
+
+print(f"Index value: {result}\n Final Name = {separate[result]}")
